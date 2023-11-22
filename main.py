83c2c84b062508552f2ef2014968a6952b848a17
Alberto Venzo <alberto.venzo95@gmail.com>
2023-11-22 21:55:57 +0100

    add command to create a database

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ import sqlite3
 
 def print_program_help():
     print("Command list for this program:")
+    print("create_new_database: create a new database file")
     print("q | quit | exit : close program")
     print("h | help : print help doc")
 
+def create_new_database():
+    database_name = input("Please enter a name from the database:")
+    database_connection = sqlite3.connect(database_name + ".db")
+    database_connection.close()
+
 close_program = False
+database_connection = None
 
 while not close_program:
     command = input("Please enter a command:")
-    if command == 'q' or command == 'quit' or command == 'exit':
+    if command == 'create_new_database':
+        create_new_database()
+    elif command == 'q' or command == 'quit' or command == 'exit':
         print("Closing program")
         close_program = True
     elif command == 'h' or command == 'help':
